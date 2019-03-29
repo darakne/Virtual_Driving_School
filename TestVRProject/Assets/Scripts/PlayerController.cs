f0d6e57b714147fa981b12dee429c961165c88bf
Eli <dracylea@gmail.com>
2019-03-29 16:55:48 +0100

    Wheel Collider Test

--- TestVRProject/Assets/Scripts/PlayerController.cs
+++ TestVRProject/Assets/Scripts/PlayerController.cs
@@ -3,23 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //this whole project was made with this tutorial: https://developer.oculus.com/documentation/unity/latest/concepts/unity-tutorial/
+//https://docs.unity3d.com/Manual/MobileInput.html
+//https://docs.unity3d.com/ScriptReference/Input.GetTouch.html
+//https://docs.unity3d.com/ScriptReference/Gyroscope.html
 public class PlayerController : MonoBehaviour
 {
-    public int speed = 0;
+    Rigidbody rigidBody;
+    public float speed = 0;
+    public float accelerationRate = 10f;
+    public float decelerationRate = 5f;
+
     // Use this for initialization
     void Start()
     {
+        rigidBody = GetComponent<Rigidbody>();
+
     }
     // Update is called once per frame
     void Update()
-    {
+    { 
+
         // get input data from keyboard or controller
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
+
         // update player position based on input
-        Vector3 position = transform.position;
-        position.x += moveHorizontal * speed * Time.deltaTime;
-        position.z += moveVertical * speed * Time.deltaTime;
-        transform.position = position;
+        Vector3 prevPos = this.transform.position;
+
+        
+        
+        float speedkm = rigidBody.velocity.magnitude;
+        //add to the current velocity according while accelerating
+        float allVelocity = speedkm + (accelerationRate * Time.deltaTime) - (decelerationRate * Time.deltaTime);
+        //subtract from the current velocity while decelerating
+        rigidBody.velocity = Vector3.forward * allVelocity * moveVertical;
+
+        // rigidBody.AddForce(new Vector3(0, -jumpHeight * 1.5f, 0), ForceMode.VelocityChange);
+        // var vel = rigidBody.velocity;      //to get a Vector3 representation of the velocity
+        // to get magnitude
+
+        if (speedkm > 1f) {
+            rigidBody.velocity.Normalize();
+        }
+            Debug.Log("speed is 1: " + speedkm);
     }
+    IEnumerator CalcVelocity()
+    {
+        while (Application.isPlaying)
+        {
+            // Position at frame start
+            Vector3 prevPos = this.transform.position;
+            // Wait till the end of the frame
+            yield return new WaitForEndOfFrame();
+            rigidBody.velocity = (prevPos - this.transform.position) / 3.6f *Time.deltaTime;
+
+            //https://answers.unity.com/questions/873859/how-to-move-an-object-a-specific-speed-in-kmh.html
+            // transform.Translate(Vector3.forward * 100 / 3.6 * Time.deltaTime);
+
+            Debug.Log("speed is 2: " + rigidBody.velocity.magnitude);
+            
+
+        }
+    }
+
 }
---
+++ TestVRProject/Assets/Scripts/SimpleCarController.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//code from here (copy-paste): https://www.gamasutra.com/blogs/VivekTank/20180706/321374/Unity_Wheel_Collider_for_Motor_vehicle_Tutorial_2018.php
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheelCollider;
+    public WheelCollider rightWheelCollider;
+    public GameObject leftWheelMesh;
+    public GameObject rightWheelMesh;
+    public bool motor;
+    public bool steering;
+}
+
+public class SimpleCarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle = 30;
+    public float brakeTorque = 1e+5f;
+    public float decelerationForce = 1e+5f;
+
+    public void ApplyLocalPositionToVisuals(AxleInfo axleInfo)
+    {
+        Vector3 position;
+        Quaternion rotation;
+        axleInfo.leftWheelCollider.GetWorldPose(out position, out rotation);
+        axleInfo.leftWheelMesh.transform.position = position;
+        axleInfo.leftWheelMesh.transform.rotation = rotation;
+        axleInfo.rightWheelCollider.GetWorldPose(out position, out rotation);
+        axleInfo.rightWheelMesh.transform.position = position;
+        axleInfo.rightWheelMesh.transform.rotation = rotation;
+    }
+
+    void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        for (int i = 0; i < axleInfos.Count; i++)
+        {
+            if (axleInfos[i].steering)
+            {
+                Steering(axleInfos[i], steering);
+            }
+            if (axleInfos[i].motor)
+            {
+                Acceleration(axleInfos[i], motor);
+            }
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Brake(axleInfos[i]);
+            }
+            ApplyLocalPositionToVisuals(axleInfos[i]);
+        }
+    }
+
+    private void Acceleration(AxleInfo axleInfo, float motor)
+    {
+        if (motor != 0f)
+        {
+            axleInfo.leftWheelCollider.brakeTorque = 0;
+            axleInfo.rightWheelCollider.brakeTorque = 0;
+            axleInfo.leftWheelCollider.motorTorque = motor;
+            axleInfo.rightWheelCollider.motorTorque = motor;
+        }
+        else
+        {
+            Deceleration(axleInfo);
+        }
+    }
+
+    private void Deceleration(AxleInfo axleInfo)
+    {
+        axleInfo.leftWheelCollider.brakeTorque = decelerationForce;
+        axleInfo.rightWheelCollider.brakeTorque = decelerationForce;
+    }
+
+    private void Steering(AxleInfo axleInfo, float steering)
+    {
+        axleInfo.leftWheelCollider.steerAngle = steering;
+        axleInfo.rightWheelCollider.steerAngle = steering;
+    }
+
+    private void Brake(AxleInfo axleInfo)
+    {
+        axleInfo.leftWheelCollider.brakeTorque = brakeTorque;
+        axleInfo.rightWheelCollider.brakeTorque = brakeTorque;
+    }
+
+}
+
